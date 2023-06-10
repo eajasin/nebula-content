c77c8b41876340a01f0612af3a43a0ed4f2763e5
eajasin <elizabethajasin@gmail.com>
2023-06-10 15:53:32 -0400

    homework

---
+++ 3-strings/0-strings-notes.js
@@ -0,0 +1,12 @@
+let language = "JavaScript"
+let adjective1 = "am really enjoying"
+let keyword1 = "(not sure)"
+let year = "1995"
+let instructor = "Devon"
+//console.log(`I am currently learning the programming language called ${language}. I ${adjective1} this program already. I read recently that declaring variables with the ${keyword1} keyword has gone out of style ever since ECMAScript ${year}. Oh, also, my instructor ${instructor} is admirable because he's self-taught!`);
+
+
+const alph = "abcdefghijklmnopqrstuvwxyz";
+           // 0123456789
+console.log(alph[4]+alph[11]+alph[8]+alph[25]+alph[0]+alph[1]+alph[4]+alph[19]+alph[7]);
+console.log(`${alph[4]}${alph[11]}${alph[8]}${alph[25]}${alph[0]}${alph[1]}${alph[4]}${alph[19]}${alph[7]}`)
