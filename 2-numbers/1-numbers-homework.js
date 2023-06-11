bf27f9dc240ebb41d0cd18b37d5df68ba00c24b4
eajasin <elizabethajasin@gmail.com>
2023-06-11 00:20:08 -0400

    message

--- 2-numbers/1-numbers-homework.js
+++ 2-numbers/1-numbers-homework.js
@@ -1,42 +1,74 @@
 
 // Remember to console.log() to debug your code and check your solutions!
-testing
+
 
 // Declare a variable and assign it to a numeric value
 // Half that value and re-assign it to the previous variable 
 
+let num1 = 100
+console.log(num1/2)
 
 // Create two negative numbers, assign them to variables and subtract one from the other together 
 
+let num2 = -5
+let num3 = -8
+console.log(num2 - num3)
 
 // Create three numbers, assign them to variables and multiply them together 
 
+let num4 = 20
+let num5 = 25
+let num6 = 30
+console.log(num4*num5*num6)
 
 // Create two even numbers, assign them to variables and divide one from the other 
 
+let num7 = 8
+let num8 = 10
+console.log(num7/num8)
 
 // Create two odd numbers, assign them to variables and find the remainder of dividing one by the other 
 
+let num9  = 13
+let num10 = 11
+console.log(num9 % num10)
 
 // find the minimum value in a set of numbers: 10,20,30,40,50 
 
+let minValue = Math.min(10,20,30,40)
+console.log(minValue)
 
 // find the maximum value in a set of numbers: -1,-2,-3,-4,-5 
 
+let maxValue = Math.max(-1,-2,-3,-4,-5)
+console.log(maxValue)
 
 // Create a variable and find it's remainder, if divided by 2
 
+let remainder = 29
+console.log(remainder%2)
 
 // find the square root of 144 
 
+let sqRoot = Math.sqrt(144)
+console.log(sqRoot)
 
 // find the value of 3 to the power of 7 
 
+let pow = Math.pow(3,7)
+console.log(pow)
 
 // find the value of 73.2 rounded 
 
+let rounded = Math.round(73.2)
+console.log(rounded)
 
 // find the rounded-down value of 9.9 
 
+let roundDown = Math.floor(9.9)
+console.log(roundDown)
 
 // find the absolute value of -1231
+
+let absV = Math.abs(-1231)
+console.log(absV)
