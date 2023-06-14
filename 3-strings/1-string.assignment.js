b93e34b807c24de0c8c1510f9a770f9d09c90def
eajasin <elizabethajasin@gmail.com>
2023-06-13 21:20:04 -0400

    message

--- 3-strings/1-string.assignment.js
+++ 3-strings/1-string.assignment.js
@@ -2,15 +2,14 @@
 // 1. Concatenate "hello" with "goodbye"
  const string1 = "hello"
  const string2  = "goodbye"
- let concatenatedstring = string1 + string2
- console.log(concatenatedstring)
+ console.log(string1.concat(string2))
+
 
 // 2. Save the substring "Friend" from the string "Hello Friend"
 const greeting1 = "Hello Friend"
 //console.log(greeting1.indexOf("F")) - output is 6
 //console.log(greeting1.indexOf("d")) //- output is 11
-console.log(greeting1.slice(6,12))
-//I get the full word when I enter 12.  Shouldn't it be 11?
+console.log(greeting1.substring(6,12))
 
 // 3. Using 5 or more of the following words, create a template literal that makes a legible sentence.
 //    Do not create your own words, use the existing ones below!
@@ -38,14 +37,15 @@ const word21 = "and";
 const word22 = "also";
 const word23 = "like"; 
 
-let legibleSentence = `The best way to ${word13} your ${word15} ${word9} is during a ${word11} game, like the Superbowl, with friends ${word21} family.`
+legibleSentence = `${word17} ${word13} ${word19} ${word15}  ${word9}.`
 console.log(legibleSentence)
 
-// 4. Given the string of your full name get your initials.
+//4. Given the string of your full name get your initials.
  
 let fullName = "Elizabeth Ajasin"
-let firstInitial = fullName.slice(0)
-let secondInitial = fullName.slice(9)
+
+let firstInitial = fullName.slice(0,1)
+let secondInitial = fullName[10]
 let myInitials = firstInitial + secondInitial
 console.log(myInitials)
 /*How do I extract a substring as an individual character (E and second occurence of A), 
@@ -55,8 +55,9 @@ not as a range?
 // 5. Get the 5th character from the following string: 'Hello World'
 
 const greeting = 'Hello World'
-console.log(greeting[5])
-//output is blank
+console.log(greeting[4])
+//REVIEW
+//THIS IS COMING OUT AS A SPACE.*
 
 // 6. Find the index of the dash from 'Coding-Time!'
 
@@ -77,16 +78,13 @@ console.log(string3.slice(0,3))
 // 9. Turn the following string into an array of words: 'This-is-a-sentence';
 
 const array = 'This-is-a-sentence'
-let stringArray = array.split('-')
-console.log(stringArray)
-/*How do I find the indices of all characters in a string? 
-How do I remove the dashes?  Doing so should lead to an array of words rather
-than an array of letters
-*/
+console.log(array.split())
+
+//adding thr split method 'removes' the dashes, leaving only an array of words.  An argument with nothing will output the sentence as is.
 
 // 10. Make the string of 12 from the following two numbers:
 
 let one = "1";
 let two = "2";
-let combinedString = one + two
-console.log(combinedString.repeat(6))
+console.log(one + two)
+
