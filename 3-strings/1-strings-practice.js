1410a6bd083892cf9e75be9dbe4b3a80fceb35f3
eajasin <elizabethajasin@gmail.com>
2023-06-14 22:12:32 -0400

    message

---
+++ 3-strings/1-strings-practice.js
@@ -0,0 +1,45 @@
+// Turn the following string into all lowercase letters
+const noCaps = 'This Should Not Have Capital Letters!'
+console.log(noCaps.toLowerCase())
+
+// Turn the following string into all uppercase letters
+const loud = "i'm very loud!";
+console.log(loud.toUpperCase())
+
+// Join the prior two variables together.
+ 
+console.log(noCaps.concat(loud))
+
+// Turn the following string into an array of names
+const stringOfNames = 'Ally Becca Carlos Drake Edgar Felicity Greta Hector';
+
+
+
+// Make your own mad-libs using a template literal
+
+
+
+// Given the name Tony Stark, get his initials
+const ironMan = "Tony Stark"
+
+
+
+// Given the string "Hey now, you're an all-star!" get the string, "all-star" and save it to a new variable
+const string = "Hey now, you're an all-star!"
+
+
+// Using the string "How are you?" extract the question mark.
+const string1 = "How are you?" 
+console.log(string1.slice[8])
+
+
+
+// Given the following variables, add them together to get 15 
+const ten = '10';
+const five = '5';
+
+
+// Given the following sentence, turn it into an array.
+
+const sentence = 'Welcome to the jungle!'; 
+
