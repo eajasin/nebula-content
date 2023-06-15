d1d4188cdf53ecffc1bfb7ca451c7504323aa6be
eajasin <elizabethajasin@gmail.com>
2023-06-14 22:54:46 -0400

    message

--- 3-strings/1-strings-practice.js
+++ 3-strings/1-strings-practice.js
@@ -13,33 +13,42 @@ console.log(noCaps.concat(loud))
 // Turn the following string into an array of names
 const stringOfNames = 'Ally Becca Carlos Drake Edgar Felicity Greta Hector';
 
-
+console.log(stringOfNames.split(' '))
 
 // Make your own mad-libs using a template literal
 
+let word1 = "ice cream"
+let word2 = "cars"
+let word3 = "friendship"
 
+console.log(`Have fun by eating ${word1}, driving fast ${word2} and maintaining a good ${word3}.`)
 
 // Given the name Tony Stark, get his initials
 const ironMan = "Tony Stark"
+let firstI = ironMan.slice(0,1)
+let secondI = ironMan[5]
+let initials = firstI + secondI
+console.log(initials)
 
-
-
-// Given the string "Hey now, you're an all-star!" get the string, "all-star" and save it to a new variable
+/*/ Given the string "Hey now, you're an all-star!" get the string, "all-star" and save it to a new variable
 const string = "Hey now, you're an all-star!"
+let newString = string.slice("all-star")
+console.log(newString)
+*/
 
-
-// Using the string "How are you?" extract the question mark.
+/*/ Using the string "How are you?" extract the question mark.
 const string1 = "How are you?" 
-console.log(string1.slice[8])
+console.log(string1.split(?))
+*/
 
 
-
-// Given the following variables, add them together to get 15 
+/*/ Given the following variables, add them together to get 15 
 const ten = '10';
 const five = '5';
+*/
 
 
 // Given the following sentence, turn it into an array.
 
 const sentence = 'Welcome to the jungle!'; 
-
+console.log(sentence.split(" "))
