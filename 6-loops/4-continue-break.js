094516b98b13291f709f52822e467c059d6d04f4
eajasin <elizabethajasin@gmail.com>
2023-06-19 10:58:51 -0400

    message

---
+++ 6-loops/4-continue-break.js
@@ -0,0 +1,31 @@
+// 1.   Write a for loop that looks for the letter a
+//      The loop should log the letter 'a' to the console
+//      STOP immediately after finding the letter 'a'
+/*
+const string = "I went to the zoo and saw two aardvarks."
+
+for(i = 0; i < string.length; i++){
+    let currentLetter = string[0]
+    if(currentLetter === "a"){
+        console.log("There's an a!")
+    break;
+    }
+}
+*/
+
+// 2.   Write a for loop that looks for a number
+//      The loop should log 'at index _ there is the number _' to the console 
+//      Stop immediately after finding that number
+//      ex: Input: ['a','b','c', 100,'e']
+//          Output: 'at index 3 there is the number 100'
+
+let foundNum = 34689869486948561
+
+
+
+
+
+// 3.   Using the continue keyword loop over an array of numbers & strings
+//      If the current element is a number skip it
+//      Otherwise add the element to an array
+//      After finishing the loop log the new array to the console
