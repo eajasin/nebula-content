b4c80df3ce4c22e6b1ed7dfb78d05673e47d8f0a
eajasin <elizabethajasin@gmail.com>
2023-06-20 22:58:28 -0400

    message

--- 6-loops/4-continue-break.js
+++ 6-loops/4-continue-break.js
@@ -1,7 +1,7 @@
 // 1.   Write a for loop that looks for the letter a
 //      The loop should log the letter 'a' to the console
 //      STOP immediately after finding the letter 'a'
-/*
+
 const string = "I went to the zoo and saw two aardvarks."
 
 for(i = 0; i < string.length; i++){
@@ -11,25 +11,25 @@ for(i = 0; i < string.length; i++){
     break;
     }
 }
-*/
+
 
 // 2.   Write a for loop that looks for a number
 //      The loop should log 'at index _ there is the number _' to the console 
 //      Stop immediately after finding that number
 //      ex: Input: ['a','b','c', 100,'e']
 //          Output: 'at index 3 there is the number 100'
-/*
-let foundNum = 34689869486948561
 
-for(i = 0; i < foundNum.length; i++){
-    let currentNumber = foundNum[0]
-    if(currentNumber === 9){
-        break;
-        let numIndex = indexof(9)
-        console.log(`at index ${numIndex} there is the number 9`)
-    }
-}
-*/
+// let foundNum = 34689869486948561
+
+// for(i = 0; i < foundNum.length; i++){
+//     let currentNumber = foundNum[0]
+//     if(currentNumber === 9){
+//         break;
+//         let numIndex = indexof(9)
+//         console.log(`at index ${numIndex} there is the number 9`)
+//     }
+// }
+
 
 
 
@@ -39,14 +39,14 @@ for(i = 0; i < foundNum.length; i++){
 //      Otherwise add the element to an array
 //      After finishing the loop log the new array to the console
 
-const newArray = ["dog", 3, 26, "cow", "chicken", 17]
-let arrayofStrings = []
+// const newArray = ["dog", 3, 26, "cow", "chicken", 17]
+// let arrayofStrings = []
 
-for(i = 0; i < newArray.length; i++){
-    let currentElement = newArray[i]
-    if(currentElement <=0 && currentElement > 0){ //can use "typeof" but how to make true/false?
-        continue;
-    } else
-        console.log(arrayofStrings.push(currentElement))
-    }
-console.log(arrayofStrings)
+// for(i = 0; i < newArray.length; i++){
+//     let currentElement = newArray[i]
+//     if(currentElement <=0 && currentElement > 0){ //can use "typeof" but how to make true/false?
+//         continue;
+//     } else
+//         console.log(arrayofStrings.push(currentElement))
+//     }
+//console.log(arrayofStrings)
