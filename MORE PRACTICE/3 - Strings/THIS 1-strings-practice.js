07a38f7ce8398ac5efc0335b38ffbe2454f7646c
eajasin <elizabethajasin@gmail.com>
2023-06-24 21:46:47 -0400

    message

---
+++ MORE PRACTICE/3 - Strings/THIS 1-strings-practice.js
@@ -0,0 +1,75 @@
+// Turn the following string into all lowercase letters
+const noCaps = 'This Should Not Have Capital Letters!'
+
+let allLower = noCaps.toLowerCase()
+
+console.log(allLower)
+
+// Turn the following string into all uppercase letters
+const loud = "i'm very loud!";
+
+let allUpper = loud.toUpperCase()
+
+console.log(allUpper)
+
+// Join the prior two variables together.
+
+let joinedString = allLower.concat(allUpper)
+
+console.log(joinedString)
+
+// Turn the following string into an array of names
+
+const stringOfNames = 'Ally Becca Carlos Drake Edgar Felicity Greta Hector';
+
+let arrayOfNames = stringOfNames.split(" ")
+console.log(arrayOfNames)
+
+//Bring these names back together.
+
+let backtoSquareOne = arrayOfNames.join(" ")
+
+// Make your own mad-libs using a template literal
+
+
+
+
+// Given the name Tony Stark, get his initials
+
+// let ironMan = "Tony Stark"
+// let split = ironMan.split(" ")
+// let initials = split.slice(0)
+
+// console.log(initials)
+
+// REVIEW HOW TO GET INITIALS
+
+
+// Given the string "Hey now, you're an all-star!" get the string, "all-star" and save it to a new variable
+
+// let lyricString = "Hey now, you're an all-star!"
+// let slicedString = lyricString.slice(19, 27)
+// console.log(slicedString)
+
+
+// Using the string "How are you?" extract the question mark.
+
+// let greeting = "How are you?"
+// let slicedString = greeting.slice(greeting.length-1)
+
+// console.log(slicedString)
+
+
+// // Given the following variables, add them together to get 15 
+
+// const ten = '10';
+// const five = '5';
+
+// console.log()
+
+// // Given the following sentence, turn it into an array.
+
+// const sentence = 'Welcome to the jungle!'; 
+
+// console.log(sentence.split(" "))
+
