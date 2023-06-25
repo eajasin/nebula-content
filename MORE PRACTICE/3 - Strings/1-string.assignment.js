62c673d917be32d173ee5360cd5c743571957c3d
eajasin <elizabethajasin@gmail.com>
2023-06-25 11:11:22 -0400

    message

--- MORE PRACTICE/3 - Strings/1-string.assignment.js
+++ MORE PRACTICE/3 - Strings/1-string.assignment.js
@@ -1,8 +1,17 @@
 
 // 1. Concatenate "hello" with "goodbye"
  
+let greeting1 = "hello"
+let greeting2 = "goodbye"
+let combinedString = greeting1.concat(" ", greeting2)
+console.log(combinedString)
 
 // 2. Save the substring "Friend" from the string "Hello Friend"
+
+let thisString = "Hello Friend"
+let subString = thisString.slice(6, 12)
+
+console.log(subString)
 
 
 
@@ -39,20 +48,39 @@ const word23 = "like";
 
 // 5. Get the 5th character from the following string: 'Hello World'
 
-
+//THIS
+// let string1 = "Hello World"
+// console.log(string1.charAt(5))
 
 // 6. Find the index of the dash from 'Coding-Time!'
 
+let string2 = "Coding-Time!"
+console.log(string2.indexOf("-"))
 
 
 // 7. Write code to grab the final letter from a any string.
 
+//THIS
+// let string3 = "Harry Potter"
+
+// console.log(string3.charAt().length-1)
 
 // 8. Write code to grab the first 3 letters from a string. 
 
 
+
 // 9. Turn the following string into an array of words: 'This-is-a-sentence';
+
+let string7 = 'This-is-a-sentence'
+let arrayOfWords = string7.split('-')
+console.log(arrayOfWords)
 
 
 // 10. Make the string of 12 from the following two numbers:
+
+let num1 = 1
+let num2 = 2
+let newNum = "" + num1 + num2 //this turns non-string variables into a combined string
+
+console.log(newNum)
 
