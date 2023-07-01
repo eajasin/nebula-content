f0b516c3a5bdecee749e82251dba5053fde809ad
eajasin <elizabethajasin@gmail.com>
2023-06-30 22:30:46 -0400

    message

--- MORE PRACTICE/4 - Control Flow/1-comparison-operators-practice.js
+++ MORE PRACTICE/4 - Control Flow/1-comparison-operators-practice.js
@@ -8,11 +8,38 @@ function greaterThan (num1, num2){
 console.log(greaterThan(3, 2))
 // Reverse the prior statement and check if 3 is less than 2
 
+function lessThan (num1, num2){
+    if(num1 < num2){
+        return true
+    } return false
+}
+console.log(lessThan(3, 2))
 
 // Check if 4 is less than or equal to 4.4
 
+function lessThanOrEqual (num1, num2){
+    if(num1 <= num2){
+        return true
+    } return false
+}
+console.log(lessThanOrEqual(4, 4.4))
 
 // Reverse the prior statement and check if 4 is greater than or equal to 4.4
 
+function greaterThanOrEqual (num1, num2){
+    if(num1 >= num2){
+        return true
+    } return false
+}
+console.log(greaterThanOrEqual(4, 4.4))
 
 // Check if the string value of 19 is greater than the numeric value of 2
+
+let stringValue = "19"
+
+function stringGreaterThanOrEqual (str, num2){
+    if(str >= num2){
+        return true
+    } return false
+}
+console.log(lessThanOrEqual(stringValue, 2))
