2dce3061dd5a55e4ad5214476ec3af29724aa41c
eajasin <elizabethajasin@gmail.com>
2023-06-30 22:18:39 -0400

    message

---
+++ MORE PRACTICE/4 - Control Flow/1-comparison-operators-practice.js
@@ -0,0 +1,18 @@
+// Write a statement that checks if 3 is greater than 2
+
+function greaterThan (num1, num2){
+    if(num1 > num2){
+        return true
+    } return false
+}
+console.log(greaterThan(3, 2))
+// Reverse the prior statement and check if 3 is less than 2
+
+
+// Check if 4 is less than or equal to 4.4
+
+
+// Reverse the prior statement and check if 4 is greater than or equal to 4.4
+
+
+// Check if the string value of 19 is greater than the numeric value of 2
---
+++ MORE PRACTICE/4 - Control Flow/2-equality-operators-practice.js
@@ -0,0 +1,19 @@
+// Create two variables, one equal to '10' and the other equal to 10
+
+
+// Comparing the two variables, are they strictly equal?
+
+
+// Comparing the two variables, are they loosely equal?
+
+
+// Comparing our two prior variables, in one statement, are they both loosely equal and strictly equal? (Using the && operator)
+
+
+// Comparing our two prior variables, in one statement, are they either loosely equal or strictly equal? (Using the || operator)
+
+
+// Check if the variable set to 10 is not loosely equal to 'ten' 
+
+
+// Create a variable and initialize it to true, check if it is not strictly equal to 'true' 
