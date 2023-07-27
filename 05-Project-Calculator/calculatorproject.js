fa25e2cb69c829dbd14e19fdc3d92dc16479d2ea
eajasin <elizabethajasin@gmail.com>
2023-07-26 21:49:31 -0400

    message

--- 05-Project-Calculator/calculatorproject.js
+++ 05-Project-Calculator/calculatorproject.js
@@ -19,7 +19,7 @@ button.addEventListener('click', e => {
 })
 })
 
-//WITH KEYDOWN
+
 
 //<-- END OF DISPLAYING CONCATENATED NUMBERS -->
 
@@ -39,8 +39,8 @@ operatorbtns.forEach(button=>{
 const splitStr = (numstr1, numstr2, separator) => {
     if(!Number.isNaN(Number(numstr1)) && !Number.isNaN(Number(numstr2))){
         return numstr1 + separator + numstr2
-    // } else {
-    //     "Error"
+     } else {
+       throw new Error("Error")
     }   
 }
 
@@ -87,40 +87,24 @@ const mathOp = (num1, num2, operation) => {
     }
 const onebtn = document.getElementById('onebtn')
 
-// const onclick = () => {
-//     = displayscreen.value
-
-// function that can split the string by the non-number
-
-
-   
-// }   
-
-// zerobtn.addEventListener('click', e => {
-//    console.log(e)
-   
-//     //resultdisplay.innerText = zerobtn.innerText
-// })
 
 
 
-
-
-// equalbtn.addEventListener('click', e => {
+equalbtn.addEventListener('click', e => {
+ const concatenatedString = document.querySelector('#displayscreen').innerText
     
-//     let result = mathOp(splitStr(num1, num2, separator))
-//     resultdisplay.innerText = result
-   
+try {
+    const separator = ' '
+    const numsWithOperator = splitStr(concatenatedString, separator)
 
+const [num1, num2, operator] = numsWithOperator.split(separator)
 
+const result = mathOp(num1, num2, operator);
+resultdisplay.innerText = result;
 
-    // let selOperator = operatorbtns.e.target.innerText
-    // let result = mathOp(number1, number2, selOperator)
-    // resultdisplay.innerText = result
-
-
-
-
+} catch (error) {
+    resultdisplay.innerText = error.message
+}
 
 })
 
@@ -129,6 +113,16 @@ const onebtn = document.getElementById('onebtn')
 
 
 
+
+    // let result = mathOp(splitStr(num1, num2, separator))
+    // resultdisplay.innerText = result
+   
+
+
+
+    // let selOperator = operatorbtns.e.target.innerText
+    // let result = mathOp(number1, number2, selOperator)
+    // resultdisplay.innerText = result
 
 
 // // <-- BACKSPACE BUTTON START -->
@@ -149,73 +143,6 @@ const onebtn = document.getElementById('onebtn')
 
 
 
-
-//<-- ADD FUNCTION START -->
-
-// const addbtn = document.getElementById('addbtn')
-
-// function add(num1, num2){
-//     return parseInt(num1) + parseInt(num2)
-// }
-
-// addbtn.addEventListener('click', () => {
-//     #displayscreen.innerText
-
-
-// })
-
-// subtractbtn.addEventListener('click', () => {
-//     const num1 = document.getElementById('num1').value
-//     const num2 = document.getElementById('num2').value
-//     const result = subtract(num1, num2)    
-//     resultdisplay.innerText = `Result: ${result}`
-// })
-
-//<-- ADD FUNCTION END -->
-
-
-
-
-
-
-
-// const arithmetic = (a, b, operator) => {
-  
-//   switch (operator){
-//   case "+":
-//      return a + b; 
-//      break;
- 
-//   case "-":
-//      return a - b; 
-  
-//   case "*":
-//      return a * b;
-  
-//   case "/":
-//      return a / b;
- 
-// }
-// }
-
-// console.log(arithmetic(20, 2, "+"))
-
-
-// Exes and Ohs (7)
-//input is string, output is boolean
-
-// const XO = (str) => {
-  
-//     let x = str.match(/x/gi) //find the number of occurrences for each letter, taking case into account
-    
-//     let o = str.match(/o/gi)
-    
-//     return (x && x.length) === (o && o.length) //determine if there is any occurrence and if the number of occurrences equals each other
-        
-//     }
-
-//Unique In Order (6)
- //input is string/array; output is string/array in original order of elements
 
 
 
