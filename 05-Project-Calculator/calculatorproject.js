2d16fe42092a960607a787804c776cdcca597cf8
eajasin <elizabethajasin@gmail.com>
2023-07-27 21:36:28 -0400

    message

--- 05-Project-Calculator/calculatorproject.js
+++ 05-Project-Calculator/calculatorproject.js
@@ -13,12 +13,13 @@ numberbtns.forEach(button =>{
 button.addEventListener('click', e => {
    if(!operatorClicked){
     num1 = num1 ? num1 + button.dataset.number : button.dataset.number
-
+   
    } else {
     num2 = num2 ? num2 + button.dataset.number : button.dataset.number
+  
    }
     document.querySelector('#displayscreen').innerText += button.dataset.number
-
+  
 
 })
 })
@@ -32,22 +33,24 @@ operatorbtns.forEach(button=>{
     button.addEventListener('click', e =>{
         operatorClicked = true
       resultdisplay.innerText += button.dataset.operator
+    separator = button.dataset.operator  
+    console.log(separator)
 })
 })
     
 //<-- END OF DISPLAYING OPERATORS -->
 
-// <-- START OF SEPARATING STRINGS -->
+// // <-- START OF SEPARATING STRINGS -->
 
-const splitStr = (num1, num2, separator) => {
-    if(!Number.isNaN(Number(num1)) && !Number.isNaN(Number(num2))){
-        return num1 + separator + num2
-     } else {
-       throw new Error("Error")
-    }   
-}
+// const splitStr = (num1, num2, separator) => {
+//     if(!Number.isNaN(Number(num1)) && !Number.isNaN(Number(num2))){
+//         return num1 + separator + num2
+//      } else {
+//        throw new Error("Error")
+//     }   
+// }
 
-// <-- END OF SEPARATING STRINGS -->
+// // <-- END OF SEPARATING STRINGS -->
 
 
 
@@ -57,68 +60,103 @@ const splitStr = (num1, num2, separator) => {
 const clearbtn = document.getElementById('clearbtn')
 
 clearbtn.addEventListener('click', e => {
-    resultdisplay.innerText = ''
+resultdisplay.innerText = ''
+num1 = null
+num2 = null
+operatorClicked = false;
+
 })
 // <-- CLEAR BUTTON END -->
 
+// <-- MAKE NEG NUMBER START -->
+
+let negnumbtn = document.getElementById('negnumbtn')
+
+negnumbtn.addEventListener('click', e => {
+    let negatedNum1 = -parseFloat(num1);
+    num1 = negatedNum1
+
+    // let negatedNum2 = -parseFloat(num2);
+    // num2 = negatedNum2
+
+    resultdisplay.innerText = negatedNum1;
+    // resultdisplay.innerText = negatedNum2
+})
+
+// <-- MAKE NEG NUMBER END -->
+
+
 
 //<-- OPERATOR FUNCTIONALITY START -->
+
 
 const mathOp = (num1, num2, operation) => {
 
     let = result;
 
-
     switch(operation) {
     case "+":
-        result = parseFloat(num1) + parseFloat (num2);
+        result = parseFloat(num1) + parseFloat(num2);
         break;
     case "-":
-        result = parseFloat(num1) - parseFloat (num2);
+        result = parseFloat(num1) - parseFloat(num2);
         break;
     case "*":
-        result = parseFloat(num1) * parseFloat (num2);
+        result = parseFloat(num1) * parseFloat(num2);
         break;
     case "/": 
-        result =  parseFloat(num1) / parseFloat (num2);
+        result = parseFloat(num1) / parseFloat(num2);
         break;
-                         
+    case "^": 
+        result = parseFloat(num1) ** parseFloat(num2);
+        break;
+    case "√":
+        result =  Math.sqrt(num1)
+    case "%":
+    result =  num1 * .1
+                             
 } return result
-    }
+}
 
     //<-- OPERATOR FUNCTIONALITY END -->
 
     //<-- EQUAL BUTTON START HERE -->
 
-    let equalbtn = document.getElementById('equalbtn')
+let equalbtn = document.getElementById('equalbtn')
 
 equalbtn.addEventListener('click', e => {
- const concatenatedString = document.querySelector('#displayscreen').innerText
-    //console.log(concatenatedString)
-
+ 
 try {
-    const separator = ' '
-    const numsWithOperator = splitStr(concatenatedString, separator)
     
-const [num1, num2, operator] = numsWithOperator.split(separator)
-
-const result = mathOp(num1, num2, operator);
+const result = mathOp(num1, num2, separator);
 resultdisplay.innerText = parseFloat(result);
 
-} catch (error) {
-    resultdisplay.innerText = error.message;
-}
+ } catch (error) {
+     resultdisplay.innerText = error.message;
+ }
 
 })
 
-//<-- EQUAL BUTTON END HERE -->
+//<-- EQUAL BUTTON END HERE --> 
 
 
+  
+// let powerbtn = document.getElementById('equalbtn')
 
+// powerbtn.addEventListener('click', e => {
+ 
+// try {
+    
+// const result = mathOp(num1, num2, separator);
+// resultdisplay.innerText = parseFloat(result);
 
+//  } catch (error) {
+//      resultdisplay.innerText = error.message;
+//  }
 
+// })
 
-
+ 
 
     // let result = mathOp(splitStr(num1, num2, separator))
     // resultdisplay.innerText = result
@@ -148,6 +186,12 @@ backspacebtn.addEventListener('click', backspace);
 
 // // <-- BACKSPACE BUTTON END -->
 
+
+// <-- ON/OFF BUTTON START -->
+
+
+
+// <-- ON/OFF BUTTON END -->
 
 
 
