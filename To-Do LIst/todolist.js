44ec0b378908f6aef2c8e38d70175791453a9747
eajasin <elizabethajasin@gmail.com>
2023-08-19 09:07:24 -0400

    message

--- To-Do LIst/todolist.js
+++ To-Do LIst/todolist.js
@@ -1,28 +1,94 @@
-const addtaskbtn = document.getElementsByClassName('ADD')
-const deletebtn = document.getElementsByClassName('delete')
-const tasksDisplay = document.getElementsByClassName('taskDisplay')
-const textbox = document.getElementsByClassName('TASKS')
-const newTask = document.getElementsByClassName('newTask')
-const taskList = document.getElementsByClassName('taskList')
+const addtaskBtn = document.querySelector('.add');
+const tasksDisplay = document.querySelector('.taskDisplay');
+const textbox = document.querySelector('.textbox');
+const taskCountDisplay = document.querySelector('.taskCountDisplay');
+const tasksCompletedDisplay = document.querySelector('.tasksCompletedDisplay');
+tasksCompletedDisplay.innerText = "Tasks Completed: 0"
+taskCountDisplay.innerText = "Task Count: 0"
 
 
-addtaskbtn.addEventListener('click', () => {
-  const text = textbox.value
 
-  if(text !== ""){
-    newTask = document.createElement("li")
+let tasksCompleted = 0;  // Move this outside of the event listener
+function incrementTasksCompleted() {
+  tasksCompleted += 1;
+  return tasksCompleted;
+}
 
-    newTask.innerHTML = 
-    //each new task should have text, button, checkbox
-    //use template literal with text of new task?
-      //`input type="text" value = ${text}` 
-    
-       
-    //adds to list
-    taskList.appendChild(newTask)
-  }  
- 
+let taskCount = 0
+function incrementNewTasks() {
+  taskCount += 1
+  return taskCount
+}
 
-})
+function decrementNewTasks() {
+  taskCount -= 1
+  return taskCount
+}
+
+
+
+
+
+addtaskBtn.addEventListener('click', (e) => {
+  e.preventDefault(); //maybe adjust this for the persistent data feature
+  const newTask = document.createElement('li');
+  let numOfTasks = incrementNewTasks()
+  taskCountDisplay.innerText = `Task Count: ${numOfTasks}`
+
+
+
+  //CHECKBOX START -->
+  let checkbox = document.createElement('input');
+  checkbox.type = "checkbox";
+  checkbox.addEventListener('click', (e) => {
+    let completed = incrementTasksCompleted();
+    tasksCompletedDisplay.innerText = `Tasks Completed: ${completed}`;
+
+
+
+  });
+
+
+  //<--CHECKBOX END
+
+  // EDIT BUTTON START -->
+  const editbtn = document.createElement('button')
+  editbtn.innerText = "Edit"
+  editbtn.addEventListener('click', (e) => {
+    newTask.innerText = ""
+    //need to be able to add a textbox here to change value
+
+  })
+
+
+  
+
+  // <-- EDIT BUTTON END
+
+  // DELETE BUTTON START -->
+  const deletebtn = document.createElement('button');
+  deletebtn.innerText = "Delete";
+  deletebtn.addEventListener('click', (e) => {
+    newTask.remove()
+    let numOfTasks = decrementNewTasks()
+    taskCountDisplay.innerText = `Task Count: ${numOfTasks}`
+    // incrementing whenever clicked, may need to add a change event listener or function
+  });
+  // <-- DELETE BUTTON END
+
+
+
+  newTask.textContent = textbox.value;
+
+  if (textbox.value !== "") {
+    tasksDisplay.appendChild(newTask);
+  }
+  newTask.appendChild(checkbox);
+  newTask.appendChild(editbtn);
+  newTask.appendChild(deletebtn)
+  textbox.value = "";
+
+
+});
 
 
