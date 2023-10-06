f7fe0e78ab16f091c9c758a6f8d725baf239b3ca
eajasin <elizabethajasin@gmail.com>
2023-10-05 23:34:23 -0400

    message

---
+++ react-router-project/src/main.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import Root, {loader as rootLoader, action as rootAction} from './routes/root.jsx'
+import './index.css'
+import ErrorPage from './routes/error-page.jsx'
+import Contact, {loader as contactLoader, } from './routes/contact.jsx'
+import EditContact, {action as editAction, } from './routes/edit.jsx'
+import { action as destroyAction } from './routes/destroy.jsx'
+
+import {
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+        loader: contactLoader
+      },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact />,
+        loader: contactLoader,
+        action: editAction,
+      },
+      {
+        path: "contacts/:contactId/destroy",
+        action: destroyAction,
+      },
+      
+    ]
+  },
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
