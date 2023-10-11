1429f239214bdfa9e86926c8d2773dd65b180da8
eajasin <elizabethajasin@gmail.com>
2023-10-10 20:28:56 -0400

    message

--- pomodoroTimer-app/src/components/startCountdown.jsx
+++ pomodoroTimer-app/src/components/startCountdown.jsx
@@ -1,35 +1,44 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 
-export default function StartCountdown(){
 
-const [countdown, setCountdown] = useState(0) //useState will be whatever the set time is
+export default function StartCountdown() {
 
-function startTimer(){
+    const [countdown, setCountdown] = useState(5) //useState will be whatever the set time is (to be input by user)
 
-    
-
-    setCountdown(5) //starting time
-
-}
-
-const timer = setInterval(() => {
-    if (countdown > 0){
-        setCountdown(countdown - 1)
-    } else {
-        clearInterval(timer)
-        setCountdown('DONE')
+    function startTimer() {
+        const interval = setInterval(() => {
+            setCountdown(prevCountdown => {
+                if (prevCountdown <= 1) {
+                    clearInterval(interval);  // Clear the interval when it reaches 0
+                    return 0;
+                }
+                return prevCountdown - 1;
+            });
+        }, 1000);
     }
-    
-}, 1000)
+
+    // useEffect(() => {
+    //     const interval = 
+    //     countdown > 0 &&
+        
+    //     setInterval(() => {
+
+    //         setInterval(setCountdown(countdown - 1))
+
+
+    //         return () => clearInterval(interval)
+    //     }, 1000)
+    // }, [countdown])
+
 
 
 
     return (
         <div>
-        {/* <div>{countdown}</div> */}
-        <button onClick={startTimer}>Start Timer</button>
-        {/* other button (settings?) for triggering time change, will need an editing/isediting thing */}
+            <div>{countdown}</div>
+            <button onClick={startTimer}>Start Timer</button>
+            {/* other button (settings?) for triggering time change, will need an editing/isediting thing */}
         </div>
     )
 }
