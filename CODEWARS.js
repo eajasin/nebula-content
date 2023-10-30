c238031b42120d81602a22c7e692d86c29d00bbb
eajasin <elizabethajasin@gmail.com>
2023-10-29 21:51:41 -0400

    message

--- CODEWARS.js
+++ CODEWARS.js
@@ -2812,22 +2812,22 @@ The Hashtag Generator (5)
 
 //Matrix Addition (6)
 
-function matrixAddition(a, b){
+// function matrixAddition(a, b){
   
-  let result = []
+//   let result = []
 
-  for(let i = 0; i < a.length; i++){
-      result[i] = []
-    for(let j = 0; j < a.length; j++){
-      result[i][j] = a[i][j] + b[i][j]
-    }
-  }
+//   for(let i = 0; i < a.length; i++){
+//       result[i] = []
+//     for(let j = 0; j < a.length; j++){
+//       result[i][j] = a[i][j] + b[i][j]
+//     }
+//   }
 
-  return result
-}
+//   return result
+// }
 
-console.log(matrixAddition([ [1, 2, 3],
-  [3, 2, 1],
-  [1, 1, 1] ],  [ [2, 2, 1],
-  [3, 2, 3],
-  [1, 1, 3] ] ))
+// console.log(matrixAddition([ [1, 2, 3],
+//   [3, 2, 1],
+//   [1, 1, 1] ],  [ [2, 2, 1],
+//   [3, 2, 3],
+//   [1, 1, 3] ] ))
